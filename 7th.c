fc07a2c6acaea81963c544228e1d584741531bc7
Chen <17109898632qq.com>
2019-12-15 19:54:38 +0800

    7th

---
+++ 7th.c
@@ -0,0 +1,3 @@
+1. 仓库级的配置文件：在仓库的 .git/.gitconfig，该配置文件只对所在的仓库有效。
+2. 全局配置文件：Mac 系统在 ~/.gitconfig，Windows 系统在 C:\Users\<用户名>\.gitconfig。
+3. 系统级的配置文件：在 Git 的安装目录下（Mac 系统下安装目录在 /usr/local/git）的 etc 文件夹中的 gitconfig
